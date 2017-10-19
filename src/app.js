f544de1c8a44944524a4120c429167e26dfb7577
Daniela Ruiz <daru015@gmail.com>
2017-10-19 10:08:55 +0200

    Hola Mundo render with React

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import ReactDOM from 'react-dom';
 // Render when document is ready
 document.addEventListener('DOMContentLoaded', function() {
     ReactDOM.render(
-
+        <div>Hola Mundo</div>,
+        document.getElementById('word-box')
     );
 });
