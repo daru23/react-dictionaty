aa3ae651f2ebcf64706f51fded7b14041012818e
Daniela Ruiz <daru015@gmail.com>
2017-11-20 17:04:07 +0100

    api call for data

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,12 +3,50 @@
  */
 import React from 'react';
 
+const api = 'http://localhost:8000/words';
+
 export default class HomePage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : {},
+            requestFailed : false
+        }
+    }
+
+    componentDidMount() {
+        fetch(api)
+            .then(response =>{
+                if (!response) {
+                    throw Error ('Network request failed.')
+                }
+            })
+            .then( d => d.json)
+            .then( d => {
+                this.setState({
+                    data: d
+                })
+            }, () => {
+                this.setState({
+                    requestFailed: true
+                })
+            })
+    }
+
     render() {
-        return (
-            <div>
-               Welcome to Dictionary
-            </div>
-        )
+        if(this.state.requestFailed) {
+            return (
+                <div>
+                    Connection Failed!
+                </div>
+            )
+        } else {
+            return (
+                <div>
+                    Welcome to Dictionary
+                </div>
+            )
+        }
     }
 }
