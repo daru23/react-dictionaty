da28adf24b0e0ce19a30ba41018574f4ebf40856
Daniela Ruiz <daru015@gmail.com>
2018-03-06 21:39:51 +0100

    example table with edit cells

--- src/components/Words.js
+++ src/components/Words.js
@@ -2,11 +2,33 @@
  * Created by daru on 05/03/2017.
  */
 import React from 'react'
+import BootstrapTable from 'react-bootstrap-table-next';
+import cellEditFactory from 'react-bootstrap-table2-editor';
+
+const columns = [{
+    dataField: 'id',
+    text: 'Product ID'
+}, {
+    dataField: 'name',
+    text: 'Product Name'
+}, {
+    dataField: 'price',
+    text: 'Product Price'
+}];
+
+const products = [
+    {id: 1, name: "daniela", price:23}
+];
 
 export default class Words extends React.Component {
     render() {
         return (
-            <div>Words</div>
+            <BootstrapTable
+                keyField="id"
+                data={ products }
+                columns={ columns }
+                cellEdit={ cellEditFactory({ mode: 'click' }) }
+            />
         )
     }
 }
